6a9e606ed2453ba0bbfadfa4877af6cc41dcf06e
JustaLiar <justa.liar@me.com>
2012-11-04 21:39:33 +0800

    Sub-classed UINavigationController in order to handle best orientation in both iOS 5 & 6.

---
+++ lifeWords/lifeWordsNavigationControllerOrientation.h
@@ -0,0 +1,13 @@
+//
+//  lifeWordsNavigationControllerOrientation.h
+//  lifeWords
+//
+//  Created by JustaLiar on 4/11/12.
+//  Copyright (c) 2012 simpleDudes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface lifeWordsNavigationControllerOrientation : UINavigationController
+
+@end
---
+++ lifeWords/lifeWordsNavigationControllerOrientation.m
@@ -0,0 +1,49 @@
+//
+//  lifeWordsNavigationControllerOrientation.m
+//  lifeWords
+//
+//  Created by JustaLiar on 4/11/12.
+//  Copyright (c) 2012 simpleDudes. All rights reserved.
+//
+
+#import "lifeWordsNavigationControllerOrientation.h"
+
+@interface lifeWordsNavigationControllerOrientation ()
+
+@end
+
+@implementation lifeWordsNavigationControllerOrientation
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (BOOL)shouldAutorotate {
+    
+    return [self.visibleViewController shouldAutorotate];
+}
+
+- (NSUInteger)supportedInterfaceOrientations {
+    return [self.visibleViewController supportedInterfaceOrientations];
+}
+
+
+
+@end
