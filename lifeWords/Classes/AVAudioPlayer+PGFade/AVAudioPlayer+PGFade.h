773266bf428c55928b8ae943af232b38fe54db9b
JustaLiar <justa.liar@me.com>
2012-11-14 21:51:05 +0800

    Implemented audio player with fading effect. Work like a charm

---
+++ lifeWords/Classes/AVAudioPlayer+PGFade/AVAudioPlayer+PGFade.h
@@ -0,0 +1,25 @@
+//
+//  AVAudioPlayer+PGFade.h
+//
+//  Created by JustaLiar on 14/11/12.
+//  Copyright (c) 2012 simpleDudes. All rights reserved.
+//
+
+#import <AVFoundation/AVFoundation.h>
+
+typedef void (^AVAudioPlayerFadeCompleteBlock)();
+
+
+@interface AVAudioPlayer (PGFade)
+
+@property (nonatomic,readonly) BOOL  fading;
+@property (nonatomic,readonly) float fadeTargetVolume;
+
+- (void) fadeToVolume:(float)volume duration:(NSTimeInterval)duration;
+- (void) fadeToVolume:(float)volume duration:(NSTimeInterval)duration completion:(AVAudioPlayerFadeCompleteBlock)completion;
+
+- (void) stopWithFadeDuration:(NSTimeInterval)duration;
+- (void) playWithFadeDuration:(NSTimeInterval)duration;
+- (void) playWithFadeDuration:(NSTimeInterval)duration atTime:(NSTimeInterval)time;
+- (void) playWithFadeDurationWithDelay:(NSTimeInterval)duration delay:(NSTimeInterval)time;
+@end
